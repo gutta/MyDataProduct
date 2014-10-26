31ccdca9c33897297575171e24975367dc69d8bb
gutta <pradeepgutta@gmail.com>
2014-10-26 10:43:02 -0400

    new file:   ll.rda 	new file:   server.R 	new file:   ui.R

---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+shinyUI(
+  fluidPage(
+    titlePanel("Over Billing Mulltiple Based on Medicare Payments - Inpatient Care"),
+    titlePanel("First column - dollars billed per paid dollar"),
+    p(a("More information here",href="https://github.com/gutta/MyDataProduct/blob/master/README.md", target="_blank")), 
+    # Inputs
+    fluidRow(
+             selectInput("st", 
+                         "Select State:", selected = "CA",
+                        c("AL","AK", "AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"))
+         
+    ),
+   
+    #  Table output.
+    
+      dataTableOutput(outputId="table"),
+    titlePanel("How Do These Numbers Correlate to Bankruptcy in Non-Medicare Population?...")   
+  )  
+)
